d741384201bfcb177f242ae0fc2f548737d081e0
Veronica Bustabad <vbustabad@gmail.com>
2018-10-07 04:02:25 +0000

    automatically backed up by learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -27,7 +27,7 @@ class GamesController < ApplicationController
 
   def update
     @game = Game.find(params[:id])
-    @game = Game.update(state: params[:state])
+    @game.update(state: params[:state])
     @game.save
     redirect_to game_path(@game)
   end
